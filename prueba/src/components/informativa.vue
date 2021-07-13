9493b3a339181a96fa4dbbe618ce5a77e224e54b
sofis <maria.landavazo@potros.itson.edu.mx>
2021-07-12 18:22:24 -0700

    Informativa 1.2

--- prueba/src/components/informativa.vue
+++ prueba/src/components/informativa.vue
@@ -1,8 +1,8 @@
 <template>
 
 <v-container>
-<v-flex>
-    <div>
+<v-flex lg>
+    <div class="d-none d-lg-block">
         <v-card
             max-width="1400"
             outlined
@@ -37,11 +37,88 @@
             </v-card-actions>
 
             </v-card>
-
+            </div>
+            </v-flex>
         <br>
-  
+
+    <!-- Despues de este div, es la version de vista para pantallas menores a LG -->
+    <v-flex class="d-lg-none d-md-flex">
+    <div>
+        <v-card
+        
+        max-width="1400"
+        outlined
+        class="mx-auto"
+    >
+        <v-list-item three-line>
+
+        <v-list-item-content>
+
+            <div class="text-overline mb-4">
+                Adultos Mayores
+            </div>
+
+        <v-list-item-title class="body-1 "><strong>
+            Envejecimiento Activo - Curso
+        </strong>
+        </v-list-item-title>
+
+        <v-list-item-subtitle class="body-1">
+                 Nuestra sociedad ha obtenido grandes éxitos con los avances científicos  y
+        </v-list-item-subtitle>
+
+         <v-list-item-subtitle class="body-1">
+               tecnológicos de las últimas décadas.  
+        </v-list-item-subtitle>
+
+        </v-list-item-content>
+
+        </v-list-item>
+
+        <v-card-actions>
+        <v-btn class="ma-2" rounded large outlined color="pink" href="http://edu.educacionbienestaritson.mx/cursos/?redirect=0">IR A CURSOS</v-btn>
+        </v-card-actions>
+        
+        </v-card>
+        <br>
+    
+        <v-card
+            max-width="1400"
+            outlined
+            class="mx-auto"
+        >
+        
+        <v-list-item three-line>
+        <v-list-item-content>
+            <div class="text-overline mb-4">
+            Educacion - Alumnos
+            </div>
+        <v-list-item-title class="body-1 "><strong>
+            Cursos Educativos Para Alumnos
+        </strong>
+        </v-list-item-title>
+
+        <v-list-item-subtitle class="body-1">
+                 Nuestra sociedad ha obtenido grandes éxitos con los avances científicos  y
+        </v-list-item-subtitle>
+
+        <v-list-item-subtitle class="body-1">
+               tecnológicos de las últimas décadas.  
+        </v-list-item-subtitle>
+
+        </v-list-item-content>
+
+        </v-list-item>
+
+        <v-card-actions>
+        <v-btn class="ma-2" rounded large outlined color="pink" href="http://edu.educacionbienestaritson.mx/cursos/?redirect=0">IR A CURSOS</v-btn>
+        </v-card-actions>
+        </v-card>
+    
     </div>
-</v-flex>
+  </v-flex>
+  
+
 </v-container>
 </template>
 
